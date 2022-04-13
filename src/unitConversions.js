bc93b6d3194fca58064b9d6a1948bc2d4c62f4a4
ericwang401 <eric.wang401@gmail.com>
2022-04-13 20:49:20 +0000

    Add unit conversions (not done)

---
+++ src/unitConversions.js
@@ -0,0 +1,9 @@
+const ingredients = [
+    {name: 'all-purpose flour', us: '1 c', metric: '142 g'},
+    {name: 'cake flour', us: '1 c', metric: '113 g'},
+    {name: 'granulated sugar', us: '1 c', metric: '198 g'},
+    {name: 'brown sugar', us: '1 c', metric: '198 g'},
+    {name: 'powdered sugar', us: '1 c', metric: '113 g'},
+]
+
+const
